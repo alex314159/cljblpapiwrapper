9bbc0a74f150df562787c59db938668a1288325e
alex314159 <alex314159@users.noreply.github.com>
2019-01-30 16:58:14 +0000

    Add files via upload

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljblpapiwrapper "0.1.0-SNAPSHOT"
+(defproject cljblpapiwrapper "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/alex314159/cljblpapiwrapper"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
