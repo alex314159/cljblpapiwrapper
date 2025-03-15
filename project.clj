ced601c9931d81e84c178c709bcd83e801e40bc7
Alexandre Almosni <alexandre.almosni@gmail.com>
2025-03-15 21:56:23 +0000

    0.3.6: update to blpapi 3.25.2.1 and refactoring

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljblpapiwrapper "0.3.6"
-  :description "Simple Clojure wrapper around the Bloomberg Java API"
+  :description "Simple Clojure wrapper for the Bloomberg Java API"
   :url "https://github.com/alex314159/cljblpapiwrapper"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
