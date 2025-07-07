b6feb76a477670f927b43352fd627d3acb5f79b5
Alexandre Almosni <alexandre.almosni@gmail.com>
2025-07-07 14:44:45 +0100

    3.6.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljblpapiwrapper "0.3.6"
+(defproject cljblpapiwrapper "0.3.6.1"
   :description "Simple Clojure wrapper for the Bloomberg Java API"
   :url "https://github.com/alex314159/cljblpapiwrapper"
   :license {:name "EPL-2.0"
